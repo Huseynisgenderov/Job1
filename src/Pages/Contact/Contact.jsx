74de5171891c4e2ce5ec4075a4fb06488eb1870e
Huseyn Isgenderov <117466905+Huseynisgenderov@users.noreply.github.com>
2023-10-13 15:23:04 +0400

    finish

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -12,17 +12,25 @@ const Contact = () => {
     <>
       <div className="littleHero">
         <div className="littleContent">
-          <div className="subText" data-aos="fade-up">Contact</div>
+          <div className="subText" data-aos="fade-up">
+            Contact
+          </div>
           <h2 data-aos="fade-up">Contact with us</h2>
         </div>
       </div>
       <section className="contact">
         <div className="contactContent">
           <div className="contactHead">
-            <div className="subText" data-aos="fade-up">Contact</div>
+            <div className="subText" data-aos="fade-up">
+              Contact
+            </div>
             <h2 data-aos="fade-up">Get in touch with us</h2>
             <p data-aos="fade-up">
-            In our contact section, feel free to reach out if you have any inquiries, wish to join our esteemed influencer team, or explore our specialized promotion services tailored for crypto platforms. Your journey in the world of cryptocurrencies begins with a single message to us.
+              In our contact section, feel free to reach out if you have any
+              inquiries, wish to join our esteemed influencer team, or explore
+              our specialized promotion services tailored for crypto platforms.
+              Your journey in the world of cryptocurrencies begins with a single
+              message to us.
             </p>
           </div>
           <div className="contactItems">
@@ -32,11 +40,6 @@ const Contact = () => {
               </span>
               <div className="telegram_links" data-aos="fade-up">
                 <p>Our Agents on Telegram:</p>
-                <p className="link_item">
-                  <Link to="https://t.me/CryptoFastAgent">
-                    @CryptoFastAgent
-                  </Link>
-                </p>
                 <p className="link_item">
                   <Link to="https://t.me/CryptoFastYT">@CryptoFastYT</Link>
                 </p>
@@ -56,8 +59,18 @@ const Contact = () => {
           <div className="contactForm">
             <form>
               <div className="form-group">
-                <input className="name" type="text" placeholder="Your Name*" data-aos="fade-up"/>
-                <input className="email" type="email" placeholder="Email*" data-aos="fade-up"/>
+                <input
+                  className="name"
+                  type="text"
+                  placeholder="Your Name*"
+                  data-aos="fade-up"
+                />
+                <input
+                  className="email"
+                  type="email"
+                  placeholder="Email*"
+                  data-aos="fade-up"
+                />
               </div>
               <div className="message" data-aos="fade-up">
                 <textarea
@@ -67,7 +80,12 @@ const Contact = () => {
                   placeholder="Message*"
                 ></textarea>
               </div>
-              <input type="submit" className="submit" value="Submit Message" data-aos="fade-up"/>
+              <input
+                type="submit"
+                className="submit"
+                value="Submit Message"
+                data-aos="fade-up"
+              />
             </form>
           </div>
         </div>
